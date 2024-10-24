9b3ef27b5a80f96dfd5b29bf36dc19df54292c4c
golova-m <golova.m@bk.ru>
2024-10-24 12:30:08 +0400

    add task(post)

--- app/main.py
+++ app/main.py
@@ -1,25 +1,25 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel
+import models.models as model
 
 
 app = FastAPI()
-
-
-class User(BaseModel):
-    username: str
-    message: str  
-
+user1 = model.User(id = 1, name = 'Ann', age = 20)
 
 @app.get('/')
 async def root():
     return {'message': 'Hello, FastAPI!' }
 
 
-@app.get("/custom")
-def root_html():
-    return {"message": "This is a custom message!"}
+@app.get("/users")
+def root_users():
+    return user1
+
+def age_verification(age: int):
+    if age >= 18: return True
+    return False
 
 
-@app.post('/')
-async def root(user: User):
-    return user
+@app.post('/user')
+def post_user(user: model.User):
+    return {'name': user.name, 'age': user.age, 'is_adult': age_verification(user.age)}
--- app/models/models.py
+++ app/models/models.py
@@ -6,19 +6,5 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     id: int
-    name: str = 'John Doe'
-    signup_ts: Union[datetime, None] = None
-    friends: List[int] = []
-
-
-# Внешние данные, имитирует входящий JSON
-external_data = {
-    "id": "123",
-    "signup_ts": "2017-06-01 12:22",
-    "friends": [1, "2", b"3"],
-}
-
-# имитируем распаковку входящих данных в коде приложения
-user = User(**external_data)
-print(user)
-print(user.id)
+    name: str
+    age: int
