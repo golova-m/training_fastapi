13ca034d6e77daed4bf9dfd2b1d4b68ce0bb763d
golova-m <golova.m@bk.ru>
2024-10-24 11:28:03 +0400

    New task

--- app/main.py
+++ app/main.py
@@ -1,22 +1,13 @@
 from fastapi import FastAPI 
-from fastapi.responses import FileResponse
-from pydantic import BaseModel
-
 
 app = FastAPI()
 
 
 @app.get('/')
 async def root():
-    return FileResponse('templates/index.html')
+    return {'message': 'Hello, FastAPI!' }
 
 
-@app.get("/file", response_class = FileResponse)
+@app.get("/custom")
 def root_html():
-    return 'templates/index.html'
-
-
-@app.post('/calculate')
-def calculate(num1: int, num2: int):
-    print({"result": num1 + num2})
-    return {"result": num1 + num2}
+    return {"message": "This is a custom message!"}
